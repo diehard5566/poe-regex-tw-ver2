e59549d585666f446b13784a421b3c40c0c7794c
shihyao001 <shihyao001@ibears.io>
2025-12-22 22:02:54 +0800

    修正router，加上nginx

--- server/routes/index.v1.js
+++ server/routes/index.v1.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = new express.Router();
 
 router.use('/maps', require('./maps'));
-router.use('/items', require('./items'));
 router.use('/scarabs', require('./scarabs'));
 
 module.exports = router;
